09982313045191b43e6495ad7ec8456f47babdc8
Anson CY <86970988+CYAnson@users.noreply.github.com>
2021-07-31 21:52:33 +0800

    googCloud_update

--- googCloud.py
+++ googCloud.py
@@ -10,16 +10,25 @@ class googCloud():
     credentials = ServiceAccountCredentials.from_json_keyfile_name(r'E:\Github\PyTrading\PyTrading/resounding-net-291613-c7c0f14f4421.json',scope)
     goog_credentials = gspread.authorize(credentials)
 
-    def get_goog_SheetValue(self, goog_spread, goog_slide):
+    def get_goog_SheetValue(self, goog_spread, goog_slide, cell):
         gc = self.goog_credentials
         sh = gc.open(goog_spread)
         ws = sh.worksheet(goog_slide)
-        cell_value = ws.acell('B2').value
+        cell_value = ws.acell(cell).value
         print(cell_value)
         return cell_value
 
-
+    def write_cell(self, goog_spread, goog_slide, cell):
+        gc = self.goog_credentials
+        sh = gc.open(goog_spread)
+        ws = sh.worksheet(goog_slide)
+        print('Please enter %: ')
+        text = input()
+        ws.update_acell(cell, text)
+        return
 
 if __name__ == "__main__":
     gc = googCloud()
-    gc.get_goog_SheetValue("程式交易紀錄", "Home")
+    gc.write_cell("程式交易紀錄", "Home", 'B3')
+    gc.write_cell("程式交易紀錄", "Home", 'C3')
+    gc.get_goog_SheetValue("程式交易紀錄", "Home", 'D3')
